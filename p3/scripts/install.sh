d3ccac98733dee5aec97cbb272f52a0c26eacaeb
Axel Coezard <axel.coezard@gmail.com>
2023-12-31 15:10:31 +0100

    add colors and fix curl

--- p3/scripts/install.sh
+++ p3/scripts/install.sh
@@ -1,20 +1,26 @@
-echo "---------------------------------------- DOCKER ----------------------------------------------------"
+#!/bin/bash
+
+PURPLE="\e[1;35m"
+YELLOW="\e[1;33m"
+ENDCOLOR="\e[0m"
+
+echo -e "${PURPLE}---------------------------------------- ${YELLOW}DOCKER${PURPLE} ----------------------------------------------------${ENDCOLOR}"
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh ./get-docker.sh --dry-run
 rm get-docker.sh
 
-echo "---------------------------------------- K3D ----------------------------------------------------"
+echo -e "${PURPLE}---------------------------------------- ${YELLOW}K3D${PURPLE} ----------------------------------------------------${ENDCOLOR}"
 
 curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
 
-echo "---------------------------------------- KUBECTL ----------------------------------------------------"
+echo -e "${PURPLE}---------------------------------------- ${YELLOW}KUBECTL${PURPLE} ----------------------------------------------------${ENDCOLOR}"
 
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 rm kubectl
 
-echo "---------------------------------------- ARGOCD CLI ----------------------------------------------------"
+echo -e "${PURPLE}---------------------------------------- ${YELLOW}ARGOCD CLI${PURPLE} ----------------------------------------------------${ENDCOLOR}"
 
-curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
+curl -Lo argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
 sudo install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
 rm argocd-linux-amd64
