2c0a229c67f1c3a5be8ce80e04d5cb50b02a48f7
Axel Coezard <axel.coezard@gmail.com>
2023-12-31 15:12:48 +0100

    add more colors

--- p3/scripts/start.sh
+++ p3/scripts/start.sh
@@ -1,18 +1,24 @@
-echo "--------- Creating \"iot\" cluster---------"
+#!/bin/bash
+
+PURPLE="\e[1;35m"
+YELLOW="\e[1;33m"
+ENDCOLOR="\e[0m"
+
+echo -e "${PURPLE}--------- ${YELLOW}Creating \"iot\" cluster ${PURPLE}---------${ENDCOLOR}"
 
 sudo k3d cluster delete iot
 sudo k3d cluster create iot
 
-echo "--------- Creating namespaces ---------"
+echo -e "${PURPLE}--------- ${YELLOW}Creating namespaces ${PURPLE}---------${ENDCOLOR}"
 
 sudo kubectl create namespace argocd
 sudo kubectl create namespace dev
 
-echo "--------- Installing argocd ---------"
+echo -e "${PURPLE}--------- ${YELLOW}Installing argocd ${PURPLE}---------${ENDCOLOR}"
 
 sudo kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
-echo "--------- Waiting for all pods to start. ---------"
+echo -e "${PURPLE}--------- ${YELLOW}Waiting for all pods to start. ${PURPLE}---------${ENDCOLOR}"
 
 ready=$(sudo kubectl get pods -n argocd | grep Running | wc -l)
 
@@ -23,12 +29,12 @@ do
     ready=$(sudo kubectl get pods -n argocd | grep Running | wc -l)
 done
 
-echo "--------- All pods started. ---------"
-echo "--------- Forwarding server port ---------"
+echo -e "${PURPLE}--------- ${YELLOW}All pods started. ${PURPLE}---------${ENDCOLOR}"
+echo -e "${PURPLE}--------- ${YELLOW}Forwarding server port ${PURPLE}---------${ENDCOLOR}"
 
 sudo kubectl port-forward -n argocd svc/argocd-server 8080:443 &
 
-echo "--------- You can now connect on localhost:8080 ---------"
-echo "--------- Default credentials : admin:$(sudo kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d) ---------"
+echo -e "${PURPLE}--------- ${YELLOW}You can now connect on localhost:8080 ${PURPLE}---------${ENDCOLOR}"
+echo -e "${PURPLE}--------- ${YELLOW}Default credentials : admin:$(sudo kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d) ${PURPLE}---------${ENDCOLOR}"
 
 argocd app create wil --repo https://github.com/Kalinololo/asebrech-iot-deploy --dest-server https://kubernetes.default.svc --dest-namespace argocd
