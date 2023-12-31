1c2765e334b1b1537ff72f49a2ea24911ed76a57
Axel Coezard <axel.coezard@gmail.com>
2023-12-31 15:21:13 +0100

    changed docker installation script

--- p3/scripts/install.sh
+++ p3/scripts/install.sh
@@ -5,9 +5,19 @@ YELLOW="\e[1;33m"
 ENDCOLOR="\e[0m"
 
 echo -e "${PURPLE}---------------------------------------- ${YELLOW}DOCKER${PURPLE} ----------------------------------------------------${ENDCOLOR}"
-curl -fsSL https://get.docker.com -o get-docker.sh
-sudo sh ./get-docker.sh --dry-run
-rm get-docker.sh
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 echo -e "${PURPLE}---------------------------------------- ${YELLOW}K3D${PURPLE} ----------------------------------------------------${ENDCOLOR}"
 
